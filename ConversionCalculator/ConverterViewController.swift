ea29f50dca170ba8425287da60f45d443e61d6ee
Joshua McEwen <jamyt7@mail.missouri.edu>
2019-04-08 17:07:54 -0500

    base of UI

---
+++ ConversionCalculator/ConverterViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ConverterViewController.swift
+//  ConversionCalculator
+//
+//  Created by McEwen, Joshua A. (MU-Student) on 4/8/19.
+//  Copyright © 2019 McEwen, Joshua A. (MU-Student). All rights reserved.
+//
+
+import UIKit
+
+class ConverterViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
